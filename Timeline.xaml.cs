844535bced5c5a6210ca9986b2a9e79153f49e4c
CatalinTermure <cataliin2002@gmail.com>
2020-02-12 17:33:12 +0200

    Finished Timeline

--- Timeline.xaml.cs
+++ Timeline.xaml.cs
@@ -18,17 +18,17 @@ namespace MathIsEZ
     /// </summary>
     public partial class Timeline : UserControl
     {
-        private int startTime, currentTime = 10;
+        private int startTime, currentTime = 0;
         private int gradations = 30;
         private const int scale = 10;
-        private double startX = 50;
+        private const double startX = 50, startY = 20;
 
         public int Gradations
         {
             get => gradations;
             set
             {
-                if(value > 5)
+                if(value > 5 && value < 100)
                 {
                     gradations = value;
                     VisualCanvas.InvalidateVisual();
@@ -53,6 +53,7 @@ namespace MathIsEZ
             {
                 currentTime = value;
                 VisualCanvas.InvalidateVisual();
+                ((Parent as Grid).Parent as LessonCreator).LessonCanvas.InvalidateVisual();
             }
         }
 
@@ -68,13 +69,29 @@ namespace MathIsEZ
             Pen smallLinePen = new Pen(new SolidColorBrush(Color.FromArgb(255, 16, 16, 16)), 0.5);
             double step = (ActualWidth - 2 * startX) / Gradations;
 
-            dc.DrawRectangle(new SolidColorBrush(Color.FromArgb(255, 72, 72, 72)), new Pen(Brushes.Black, 0), new Rect(new Point(startX, 0), new Size(ActualWidth - 2 * startX, ActualHeight)));
+            dc.DrawRectangle(new SolidColorBrush(Color.FromArgb(255, 72, 72, 72)), new Pen(Brushes.Black, 0), new Rect(new Point(startX, startY), new Size(ActualWidth - 2 * startX, ActualHeight)));
 
-            for(int i = 1; i <= Gradations; i++)
+            dc.DrawText(new FormattedText("0", System.Globalization.CultureInfo.GetCultureInfo("en-us"),
+                    FlowDirection.LeftToRight, new Typeface("Futura"), (11 - Gradations / 30) * ActualWidth / 1570d, Brushes.White, VisualTreeHelper.GetDpi(this).PixelsPerDip),
+                    new Point(startX - 5, startY - 15 + Gradations / 30));
+
+            for (int i = 1; i <= Gradations; i++)
             {
-                dc.DrawLine(linePen, new Point(startX + step * i, 0), new Point(startX + step * i, ActualHeight));
-                dc.DrawLine(smallLinePen, new Point(startX + step * i - step / 2, 0), new Point(startX + step * i - step / 2, ActualHeight));
+                dc.DrawText(new FormattedText($"{i * scale}", System.Globalization.CultureInfo.GetCultureInfo("en-us"),
+                    FlowDirection.LeftToRight, new Typeface("Futura"), (11 - Gradations / 30) * ActualWidth / 1570d, Brushes.White, VisualTreeHelper.GetDpi(this).PixelsPerDip),
+                    new Point(startX + step * i - 10, startY - 15 + Gradations / 30));
+                dc.DrawLine(linePen, new Point(startX + step * i, startY), new Point(startX + step * i, ActualHeight));
+                dc.DrawLine(smallLinePen, new Point(startX + step * i - step / 2, startY), new Point(startX + step * i - step / 2, ActualHeight));
             }
+
+            if(CurrentTime % scale != 0)
+            {
+                dc.DrawText(new FormattedText(CurrentTime.ToString(), System.Globalization.CultureInfo.GetCultureInfo("en-us"),
+                    FlowDirection.LeftToRight, new Typeface("Futura"), (11 - Gradations / 30) * ActualWidth / 1570d, Brushes.White, VisualTreeHelper.GetDpi(this).PixelsPerDip),
+                    new Point(startX - 5 + ((double)CurrentTime / scale) * step, startY - 15));
+            }
+
+            dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(startX + ((double)CurrentTime / scale) * step, startY), new Point(startX + ((double)CurrentTime / scale) * step, ActualHeight));
         }
 
         public Timeline()
@@ -92,6 +109,8 @@ namespace MathIsEZ
         private void Timeline_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             Point location = e.GetPosition(this);
+
+            CurrentTime = (int)Math.Round(Math.Min(ActualWidth - 2 * startX, Math.Max(0d, (location.X - startX))) / (ActualWidth - (2 * startX)) * Gradations * scale);
         }
 
         private void Timeline_MouseWheel(object sender, MouseWheelEventArgs e)
